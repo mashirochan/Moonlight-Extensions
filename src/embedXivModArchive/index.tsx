e08f4a4046a014fdef6011554f5573eeeec10ebd
Mashiro-chan <11561006+mashirochan@users.noreply.github.com>
2026-03-09 21:24:46 -0400

    first commit

---
+++ src/embedXivModArchive/index.tsx
@@ -0,0 +1,17 @@
+import { ExtensionWebExports } from "@moonlight-mod/types";
+
+// https://moonlight-mod.github.io/ext-dev/webpack/#webpack-module-insertion
+export const webpackModules: ExtensionWebExports["webpackModules"] = {
+  embedXivModArchive: {
+    dependencies: [
+      {
+        id: "react"
+      }
+    ],
+    entrypoint: true
+  },
+
+  someLibrary: {
+    // Keep this object, even if it's empty! It's required for the module to be loaded.
+  }
+};
